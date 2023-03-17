c6c6e229fb746c3ecf02b022dc0e8499735e9c1f
Sergey L <lazarev171193@mail.ru>
2023-03-17 17:28:31 +0400

    fix postValidationMiddleware (blogId)

--- src/middlewares/validation/posts/posts-validation.middleware.ts
+++ src/middlewares/validation/posts/posts-validation.middleware.ts
@@ -1,13 +1,7 @@
-import {body, CustomValidator, param} from "express-validator";
+import {body, CustomValidator} from "express-validator";
 import {blogsLocalRepository} from "../../../repositories/blogs-repository";
 import {inputValidationMiddleware} from "../input-validation-middleware";
 
-const titleValidation = body('title').isString().trim().notEmpty().isLength({max: 30})
-const shortDescriptionValidation = body('shortDescription').isString().trim().notEmpty().isLength({max: 100})
-const contentValidation = body('content').isString().trim().notEmpty().isLength({max: 1000})
-const blogIdValidation = body('blogId').isString().trim().notEmpty()
-
-// для валидации blogId (проверки наличия такого блога) при создании поста
 const isValidBlogId: CustomValidator = value => {
     if(!value) {
         return new Error('Invalid Blog_ID')
@@ -21,14 +15,16 @@ const isValidBlogId: CustomValidator = value => {
         }
     }
 };
-const inputBlogIdValidator = param('id').custom(isValidBlogId);
+const titleValidation = body('title').isString().trim().notEmpty().isLength({max: 30})
+const shortDescriptionValidation = body('shortDescription').isString().trim().notEmpty().isLength({max: 100})
+const contentValidation = body('content').isString().trim().notEmpty().isLength({max: 1000})
+const blogIdValidation = body('blogId').isString().trim().notEmpty().custom(isValidBlogId)
 
 export const createPostValidation = [
     titleValidation,
     shortDescriptionValidation,
     contentValidation,
     blogIdValidation,
-    inputBlogIdValidator,
     inputValidationMiddleware
 ]
 
