f2a84a2644c4de26afb236e5a271f85725b1c995
Sergey L <lazarev171193@mail.ru>
2023-03-17 17:07:52 +0400

    fix postValidationMiddleware (blogId)

--- src/middlewares/validation/posts/posts-validation.middleware.ts
+++ src/middlewares/validation/posts/posts-validation.middleware.ts
@@ -5,15 +5,21 @@ import {inputValidationMiddleware} from "../input-validation-middleware";
 const titleValidation = body('title').isString().trim().notEmpty().isLength({max: 30})
 const shortDescriptionValidation = body('shortDescription').isString().trim().notEmpty().isLength({max: 100})
 const contentValidation = body('content').isString().trim().notEmpty().isLength({max: 1000})
-const blogIdValidation = body('content').exists().isString().trim().notEmpty()
+const blogIdValidation = body('content').isString().trim().notEmpty()
 
 // для валидации blogId (проверки наличия такого блога) при создании поста
 const isValidBlogId: CustomValidator = value => {
-    const blog = blogsLocalRepository.findBlogById(value)
-    if (blog) {
-        return true;
+    if(!value) {
+        return new Error('Invalid Blog_ID')
+    } else {
+        const blog = blogsLocalRepository.findBlogById(value)
+
+        if (blog) {
+            return true;
+        } else {
+            return new Error('Invalid Blog_ID')
+        }
     }
-    return new Error('Invalid Blog_ID')
 };
 const inputBlogIdValidator = param('id').custom(isValidBlogId);
 
