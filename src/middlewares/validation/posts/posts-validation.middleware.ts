ec4bb8d144d86a9a43a9a9d0ce6b9d8c8ae85a47
Sergey L <lazarev171193@mail.ru>
2023-03-17 17:17:32 +0400

    fix postValidationMiddleware (blogId)

--- src/middlewares/validation/posts/posts-validation.middleware.ts
+++ src/middlewares/validation/posts/posts-validation.middleware.ts
@@ -1,34 +1,26 @@
-import {body, CustomValidator, param} from "express-validator";
-import {blogsLocalRepository} from "../../../repositories/blogs-repository";
+import {body} from "express-validator";
 import {inputValidationMiddleware} from "../input-validation-middleware";
+import {blogsLocalRepository} from "../../../repositories/blogs-repository";
+
+const allBlogs = blogsLocalRepository.findAllBlogs()
 
 const titleValidation = body('title').isString().trim().notEmpty().isLength({max: 30})
 const shortDescriptionValidation = body('shortDescription').isString().trim().notEmpty().isLength({max: 100})
 const contentValidation = body('content').isString().trim().notEmpty().isLength({max: 1000})
-const blogIdValidation = body('blogId').isString().trim().notEmpty()
-
-// для валидации blogId (проверки наличия такого блога) при создании поста
-const isValidBlogId: CustomValidator = value => {
-    if(!value) {
-        return new Error('Invalid Blog_ID')
-    } else {
-        const blog = blogsLocalRepository.findBlogById(value)
-
-        if (blog) {
+const blogIdValidation = body('blogId').isString().trim().notEmpty().custom(value => {
+    for (const key of allBlogs) {
+        if (+value === +key.id) {
             return true;
-        } else {
-            return new Error('Invalid Blog_ID')
         }
     }
-};
-const inputBlogIdValidator = param('id').custom(isValidBlogId);
+    throw new Error("There isn't such blogId")
+});
 
 export const createPostValidation = [
     titleValidation,
     shortDescriptionValidation,
     contentValidation,
     blogIdValidation,
-    inputBlogIdValidator,
     inputValidationMiddleware
 ]
 
