ebd7d27007ec2e041faee0c474490e6e9609dc36
Sergey L <lazarev171193@mail.ru>
2023-03-17 16:55:21 +0400

    fix authMiddleware

--- src/middlewares/authorization-middleware.ts
+++ src/middlewares/authorization-middleware.ts
@@ -6,25 +6,41 @@ const users = [
     {login: 'admin', password: 'qwerty'}
 ]
 
+let data = `${users[0].login}:${users[0].password}`
+let buff = new Buffer(data)
+let base64data = buff.toString('base64')
+
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const authHeader: string | undefined = req.header(`Authorization`);
-    if (authHeader) {
-        const authTokens = authHeader.split(` `)
-        console.log(authTokens[0]);
-        if (authTokens[0] !== 'Basic') {
-            res.sendStatus(CodeResponsesEnum.Unauthorized_401);
-            return;
-        } else {
-            let [authLogin, authPassword] = atob(authTokens[1]).split(':')
-            if (authLogin === users[0].login && authPassword === users[0].password) {
-                next();
-            } else {
-                res.sendStatus(CodeResponsesEnum.Unauthorized_401);
-                return;
-            }
-        }
+
+    let authHeader = req.headers.authorization
+    if (!authHeader) {
+        res.send(401)
+        return
+    }
+    if (authHeader && authHeader === `Basic ${base64data}`) {
+        next()
     } else {
-        res.sendStatus(CodeResponsesEnum.Unauthorized_401);
+        res.send(401)
         return;
     }
+//     const authHeader: string | undefined = req.header(`Authorization`);
+//     console.log('test',authHeader);
+//     if (authHeader) {
+//         const authTokens = authHeader.split(` `)
+//         if (authTokens[0] !== 'Basic') {
+//             res.sendStatus(CodeResponsesEnum.Unauthorized_401);
+//             return;
+//         } else {
+//             let [authLogin, authPassword] = atob(authTokens[1]).split(':')
+//             if (authLogin === users[0].login && authPassword === users[0].password) {
+//                 next();
+//             } else {
+//                 res.sendStatus(CodeResponsesEnum.Unauthorized_401);
+//                 return;
+//             }
+//         }
+//     } else {
+//         res.sendStatus(CodeResponsesEnum.Unauthorized_401);
+//         return;
+//     }
 }
