019abea1eb8e6f2a57e7af0c65ec9260bddbf3e5
Sergey L <lazarev171193@mail.ru>
2023-03-17 16:29:26 +0400

    fix authMiddleware

--- src/middlewares/authorization-middleware.ts
+++ src/middlewares/authorization-middleware.ts
@@ -10,15 +10,19 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     const authHeader: string | undefined = req.header(`Authorization`);
     if (authHeader) {
         const authTokens = authHeader.split(` `)
-        let [authLogin, authPassword] = atob(authTokens[1]).split(':')
-
-        if (authLogin === users[0].login && authPassword === users[0].password) {
-            next();
-        } else {
+        console.log(authTokens[0]);
+        if (authTokens[0] !== 'Basic') {
             res.sendStatus(CodeResponsesEnum.Unauthorized_401);
             return;
+        } else {
+            let [authLogin, authPassword] = atob(authTokens[1]).split(':')
+            if (authLogin === users[0].login && authPassword === users[0].password) {
+                next();
+            } else {
+                res.sendStatus(CodeResponsesEnum.Unauthorized_401);
+                return;
+            }
         }
-
     } else {
         res.sendStatus(CodeResponsesEnum.Unauthorized_401);
         return;
